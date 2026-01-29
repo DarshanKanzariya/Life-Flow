c020e2f9b297e1d44f4077ec173d1705cff22ded
DarshanKanzariya <kanzariyadarshan24@gamil.com>
2026-01-29 16:22:27 +0530

    Ignore env file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/userModel");
 const bcrypt = require ('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const registerController = async (req, res) => {
   try {
@@ -32,4 +33,39 @@ const registerController = async (req, res) => {
   }
 };
 
-module.exports = registerController;
+//login call back
+const loginController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).send({
+        message: "User Not Found",
+        success: false,
+      });
+    }
+    //check password
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res.status(500).send({
+        message: "Invalid Email or Password",
+        success: false,
+      });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {expiresIn: "1d",});
+    return res.status(200).send({
+      message: "Login Successful",
+      success: true,
+      user: user,
+      token: token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+        message: "Error in Login",
+        success: false,
+        error,
+    });
+  }
+};
+
+module.exports = {registerController, loginController };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const registerController = require("../controllers/authController");
+const {
+  loginController,
+  registerController
+} = require("../controllers/authController");
 
 const router = express.Router();
 
+//routes
+// register user || POST
 router.post("/register", registerController);
+
+// login user || POST
+router.post("/login", loginController);
 
 module.exports = router;
