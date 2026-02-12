91917bd113dca82ff5ce094b596b64a25cb225fe
DarshanKanzariya <kanzariyadarshan24@gamil.com>
2026-02-12 13:29:25 +0530

    Added client-side code and made modifications to inventory controllers, user model, and inventory routes.

---
+++ client/src/pages/auth/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-8">
+            <img src="./assets/images/1234.jpeg" alt="loginimage" className="" />
+        </div>
+        <div className="col-md-4">login</div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
 const {
   createInventoryController,
+  getInventoryController,
 } = require("../controllers/inventoryControllers");
 
 const router = express.Router();
@@ -9,5 +10,8 @@ const router = express.Router();
 //Routes
 //Add inventory || POST
 router.post("/create-inventory", authMiddleware, createInventoryController);
+
+//GET all records || GET
+router.get("/get-inventory", authMiddleware, getInventoryController);
 
 module.exports = router;
